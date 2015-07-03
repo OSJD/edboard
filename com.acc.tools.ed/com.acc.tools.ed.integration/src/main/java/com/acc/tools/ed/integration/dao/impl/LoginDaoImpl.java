558c68ddee8d369d5c06211ce826d91f8e54a67b
OSJD <muralee.kishna@gmail.com>
2015-07-03 11:16:00 +0530

    Update LoginDaoImpl.java
    Login changes

--- com.acc.tools.ed/com.acc.tools.ed.integration/src/main/java/com/acc/tools/ed/integration/dao/impl/LoginDaoImpl.java
+++ com.acc.tools.ed/com.acc.tools.ed.integration/src/main/java/com/acc/tools/ed/integration/dao/impl/LoginDaoImpl.java
@@ -2,10 +2,12 @@ package com.acc.tools.ed.integration.dao.impl;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.eclipse.jdt.core.dom.CatchClause;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -22,7 +24,7 @@ public class LoginDaoImpl extends AbstractEdbDao implements LoginDao{
 		
 			final Connection connection=getConnection();
 			Statement stmt=connection.createStatement();
-			final String loginQuery="SELECT P.PROJ_ID,P.PROJ_NAME, E.EMP_ID,E.EMP_EMPLOYEE_ID,E.EMP_ROLE,E.EMP_LEVEL,E.EMP_SUP_EMP_ID, E.LOGOUT, E.LASTLOGINTIME FROM "
+			final String loginQuery="SELECT P.PROJ_ID,P.PROJ_NAME, E.EMP_ID,E.EMP_EMPLOYEE_ID,E.EMP_ROLE,E.EMP_LEVEL,E.EMP_SUP_EMP_ID, E.LOGOUT, E.LASTLOGINTIME ,E.PASSWORD,E.PWDFLAG FROM "
 					+ "(EDB_MSTR_EMP_DTLS E LEFT JOIN  EDB_PROJ_EMP PE ON E.EMP_ID=PE.EMP_ID) "
 					+ " LEFT JOIN EDB_PROJECT P ON P.PROJ_ID=PE.PROJ_ID WHERE E.EMP_ENTERPRISE_ID='"+name+"'";
 			log.debug(loginQuery);
@@ -40,11 +42,41 @@ public class LoginDaoImpl extends AbstractEdbDao implements LoginDao{
 				user.setProjectName(resultSet.getString("PROJ_NAME"));
 				user.setLogout(resultSet.getBoolean("LOGOUT"));
 				user.setLastLoginTime(resultSet.getLong("LASTLOGINTIME"));
+				user.setPassword(resultSet.getString("PASSWORD"));
+				user.setPasswordFlag(resultSet.getString("PWDFLAG"));
 			};
 
 		
 		return user;
 	}
+	
+	public EDBUser searchCredentails(String name,String password) throws IOException, SQLException{
+		
+		final Connection connection=getConnection();
+		Statement stmt=connection.createStatement();
+		final String loginQuery="SELECT P.PROJ_ID,P.PROJ_NAME, E.EMP_ID,E.EMP_EMPLOYEE_ID,E.EMP_ROLE,E.EMP_LEVEL,E.EMP_SUP_EMP_ID, E.LOGOUT, E.LASTLOGINTIME FROM "
+				+ "(EDB_MSTR_EMP_DTLS E LEFT JOIN  EDB_PROJ_EMP PE ON E.EMP_ID=PE.EMP_ID) "
+				+ " LEFT JOIN EDB_PROJECT P ON P.PROJ_ID=PE.PROJ_ID WHERE E.EMP_ENTERPRISE_ID='"+name+"' , E.password = '"+password+"'";
+		log.debug(loginQuery);
+		final ResultSet resultSet = stmt.executeQuery(loginQuery);
+		EDBUser user=null;
+		while (resultSet.next()) {
+			user=new EDBUser();
+			user.setEnterpriseId(name);
+			user.setEmployeeId(resultSet.getInt("EMP_ID"));
+			user.setSapId(resultSet.getInt("EMP_EMPLOYEE_ID"));
+			user.setRole(resultSet.getString("EMP_ROLE"));
+			user.setLevel(resultSet.getString("EMP_LEVEL"));
+			user.setSupervisorId(resultSet.getInt("EMP_SUP_EMP_ID"));
+			user.setProjectId(resultSet.getInt("PROJ_ID"));
+			user.setProjectName(resultSet.getString("PROJ_NAME"));
+			user.setLogout(resultSet.getBoolean("LOGOUT"));
+			user.setLastLoginTime(resultSet.getLong("LASTLOGINTIME"));
+		};
+
+	
+	return user;
+}
 	
 	public EDBUser getEmployeeById(Integer employeeId) throws IOException, SQLException{
 		
@@ -84,6 +116,49 @@ public class LoginDaoImpl extends AbstractEdbDao implements LoginDao{
 		log.debug(updateLogoutQuery);
 		stmt.executeUpdate(updateLogoutQuery);
 	}
+
+	
+
+	public String updatePassword(String password, Integer empId)  {
+		
+		try{
+			
+		
+		final Connection connection=getConnection();
+		Statement stmt=connection.createStatement();
+		final String updatePwdQuery="UPDATE EDB_MSTR_EMP_DTLS SET PWDFLAG=? ,PASSWORD = ? WHERE EMP_ID= ?";
+		log.debug(updatePwdQuery);
+		
+		int status =0;
+		PreparedStatement  preparedStatement = getConnection().prepareStatement(updatePwdQuery);
+		preparedStatement.setString(1, "YES");
+		preparedStatement.setString(2, password);
+		preparedStatement.setInt(3, empId);
+		status=preparedStatement.executeUpdate();
+		preparedStatement.close();
+		
+		System.out.println("rows UPDATED");
+		//rowcount= selectStatement.executeUpdate(delIdQuery);
+		if(status==0)
+		{
+			System.out.println("update failure");
+
+		}
+		else{
+
+			System.out.println("update success");
+			return "success";
+		}
+		return "failure";
+	}
+		catch(Exception E)
+		{
+			E.printStackTrace();
+		}
+		return "failure";
+	}
+	
+	
 
 }
 
